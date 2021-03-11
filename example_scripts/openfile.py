60305f0f36e5fd881ef3ee6ea8a6cd78a00eaed1
Cassidy Berk <cassberk@Cassidys-MacBook-Pro.local>
2021-03-11 14:10:46 -0800

    put a new file in example scripts and told gitignore to stop tracking example scripts

--- example_scripts/openfile.py
+++ example_scripts/openfile.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog,qApp
 from PyQt5.QtGui import QIcon
+import os
 
 class App(QWidget):
 
@@ -11,38 +12,55 @@ class App(QWidget):
         self.top = 10
         self.width = 640
         self.height = 480
-        self.initUI()
-    
+        # self.initUI()
+        self.openFileNamesDialog()
+        # self.loadFiles()
+        # self.quit()
+        # self.exit()
+        sys.exit()
+
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         
-        self.openFileNameDialog()
+        # self.openFileNameDialog()
         self.openFileNamesDialog()
-        self.saveFileDialog()
-        
+        # self.saveFileDialog()
+        # self.show_file_dialog()
         self.show()
-    
-    def openFileNameDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
-        if fileName:
-            print(fileName)
-    
+
+    # def show_file_dialog(self):
+    #     selected_dir = QFileDialog.getExistingDirectory(self, caption='Choose Directory', directory=os.getcwd())
+    #     self.lineEdit_save_location.setText(selected_dir) 
+
+    # def openFileNameDialog(self):
+    #     options = QFileDialog.Options()
+    #     options |= QFileDialog.DontUseNativeDialog
+    #     fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+    #     if fileName:
+    #         print(fileName)
+    # def loadFiles(self):
+    #     filter = "All Files (*)"
+    #     file_name = QFileDialog()
+    #     file_name.setFileMode(QFileDialog.ExistingFiles)
+    #     names = file_name.getOpenFileNames(self, "Open files", "C\\Desktop", filter)
+    #     print(names[0])
+
     def openFileNamesDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
+        files = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+
+        # files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
         if files:
             print(files)
     
-    def saveFileDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
-        if fileName:
-            print(fileName)
+    # def saveFileDialog(self):
+    #     options = QFileDialog.Options()
+    #     options |= QFileDialog.DontUseNativeDialog
+    #     fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
+    #     if fileName:
+    #         print(fileName)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
