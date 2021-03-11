5d079fbe630f161c4ec229fcac065039ffb60ff2
Cassidy Berk <cassberk@Cassidys-MacBook-Pro.local>
2021-03-11 14:05:51 -0800

    got rid of folder select as well as adding message box for experiment overwriting

--- avgtohdf5.py
+++ avgtohdf5.py
@@ -1,10 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog,qApp, QDialog, QFormLayout, QLabel, QPushButton,QVBoxLayout, QListView
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog,qApp, QDialog, QFormLayout, QLabel, QPushButton,QVBoxLayout, QListView,QMessageBox
 from PyQt5.QtGui import QIcon, QStandardItem,QStandardItemModel
 import os
 sys.path.append("/Users/cassberk/code/")
 import xps_peakfit.avg
 import glob
+import h5py
 from IPython import embed as shell
 
 class MessageWindow(QWidget):
@@ -39,8 +40,6 @@ class App(QWidget):
         self.samplename = QLineEdit()
         self.samplename.setObjectName("host")
         self.samplename.setFixedWidth(180)
-
-
         self.samplenameLabel = QLabel('Sample Name', self) 
 
         self.experimentname = QLineEdit()
@@ -52,9 +51,18 @@ class App(QWidget):
         self.fileSelect.setText("Choose Files")
         self.fileSelect.clicked.connect(self.loadFiles)
 
-        self.folderSelect = QPushButton()
-        self.folderSelect.setText("Choose Folder")
-        self.folderSelect.clicked.connect(self.loadFolder)
+        # self.folderSelect = QPushButton()
+        # self.folderSelect.setText("Choose Folder")
+        # self.folderSelect.clicked.connect(self.loadFolder)
+
+        self.choosePath = QPushButton()
+        self.choosePath.setText("Choose Save Location")
+        self.choosePath.clicked.connect(self.chooseSavePath)
+
+        self.savepath = QLineEdit()
+        # self.savepath.setObjectName("host")
+        self.savepath.setFixedWidth(400)
+        self.savepathLabel = QLabel('Save Path', self) 
 
         self.convertButton = QPushButton()
         self.convertButton.setText("Convert")
@@ -77,7 +85,10 @@ class App(QWidget):
         layout.addWidget(self.experimentnameLabel)
         layout.addWidget(self.experimentname)
         layout.addWidget(self.fileSelect)
-        layout.addWidget(self.folderSelect)
+        # layout.addWidget(self.folderSelect)
+        layout.addWidget(self.choosePath)
+        layout.addWidget(self.savepathLabel)
+        layout.addWidget(self.savepath)
         layout.addWidget(self.listLabel)
         layout.addWidget(self.listView)
         layout.addWidget(self.clearButton)
@@ -91,25 +102,33 @@ class App(QWidget):
         self.files = []
         self.model.clear()
 
+    def chooseSavePath(self):
+        filter = "All Files (*)"
+        file_name = QFileDialog()
+        file_name.setFileMode(QFileDialog.ExistingFiles)
+        _files,_ = file_name.getOpenFileNames(self, "All Files (*)", "/Volumes/GoogleDrive/Shared drives/StOQD/sample_library", filter)
+        self.savepath.setText(_files[0])
+        self.samplename.setText(_files[0].split('/')[-1].split('.')[0])
+
     def loadFiles(self):
         filter = "All Files (*)"
         file_name = QFileDialog()
         file_name.setFileMode(QFileDialog.ExistingFiles)
-        _files,_ = file_name.getOpenFileNames(self, "All Files (*)", "C\\Desktop", filter)
+        _files,_ = file_name.getOpenFileNames(self, "All Files (*)", "/Volumes/GoogleDrive/Shared drives/StOQD/sample_library", filter)
         for i in _files:
             self.files.append(i)
             item = QStandardItem(i)
             self.model.appendRow(item)
 
-    def loadFolder(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        _folder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+    # def loadFolder(self):
+    #     options = QFileDialog.Options()
+    #     options |= QFileDialog.DontUseNativeDialog
+    #     _folder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
 
-        for i in glob.glob(_folder+'/*'):
-            self.files.append(i)
-            item = QStandardItem(i)
-            self.model.appendRow(item)
+    #     for i in glob.glob(_folder+'/*'):
+    #         self.files.append(i)
+    #         item = QStandardItem(i)
+    #         self.model.appendRow(item)
 
     def convert(self):
         if self.samplename.text() =='':
@@ -117,12 +136,38 @@ class App(QWidget):
             self.error.show()
             return
         elif self.experimentname.text() =='':
+            # print(self.savepath.text())
+            # print(self.savepath.text()=='')
             self.error = MessageWindow(message = 'You must name the Experiment')
             self.error.show()
+            # msg = QMessageBox()
+            # msg.setIcon(QMessageBox.Information)
+            # msg.setText("This is a message box")
+            # msg.show()
             return
 
         else:
-            xps_peakfit.avg.avg_to_hdf5(sample_name = self.samplename.text(),experiment_name = self.experimentname.text(),filepath = self.files,force = True)
+            if self.savepath.text() == '':
+                svpth = '/'+os.path.join(os.path.join(*self.files[0].split('/')[0:-1]),self.samplename.text()+'.hdf5')
+                print(svpth)
+            else:
+                svpth = self.savepath.text()
+
+            with h5py.File(svpth,'a') as f:
+                current_groups = [grp for grp in f.keys()]
+            if any([self.experimentname.text() in group for group in current_groups]):
+                choice = QMessageBox.question(self, 'Experiment already present',
+                                                "Experiment already present. \n Do you want to overwrite?",
+                                                QMessageBox.Yes | QMessageBox.No)
+                if choice == QMessageBox.Yes:
+                    print("overwriting")
+                    xps_peakfit.avg.avg_to_hdf5(sample_name = self.samplename.text(),experiment_name = self.experimentname.text(),avgfiles = self.files,savepath = svpth,force = False)
+                else:
+                    pass
+            else:
+                shell()
+                xps_peakfit.avg.avg_to_hdf5(sample_name = self.samplename.text(),experiment_name = self.experimentname.text(),avgfiles = self.files,savepath = svpth,force = False)
+               
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
